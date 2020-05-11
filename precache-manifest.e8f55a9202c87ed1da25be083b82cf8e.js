fbd4e4208324ea4597de0553c820820f815dcace
Kira Slobodina <slobodina.kira@gmail.com>
2020-05-11 22:04:55 +0100

    Updates

---
+++ precache-manifest.e8f55a9202c87ed1da25be083b82cf8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062c7cfbc78905e03678ecddd1a85584",
+    "url": "/infinite-scroll-poc/index.html"
+  },
+  {
+    "revision": "49e736257b0ccc8952f0",
+    "url": "/infinite-scroll-poc/static/css/main.935a451b.chunk.css"
+  },
+  {
+    "revision": "c8ec2f5feb100e824c86",
+    "url": "/infinite-scroll-poc/static/js/2.4ac9cf53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infinite-scroll-poc/static/js/2.4ac9cf53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e736257b0ccc8952f0",
+    "url": "/infinite-scroll-poc/static/js/main.c521e552.chunk.js"
+  },
+  {
+    "revision": "67dd058681af48258d34",
+    "url": "/infinite-scroll-poc/static/js/runtime-main.5ecc42e1.js"
+  }
+]);
