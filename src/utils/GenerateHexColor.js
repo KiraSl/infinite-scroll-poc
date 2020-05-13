a428c025713424ef429ff8c747c335fcb4638b20
Kira Slobodina <slobodina.kira@gmail.com>
2020-05-13 21:46:34 +0100

    Rewrite generateHexColor function

--- src/utils/GenerateHexColor.js
+++ src/utils/GenerateHexColor.js
@@ -1,3 +1,8 @@
 export function generateHexColor() {
-  return `#${(Math.random() * 0xFFFFFF << 0).toString(16)}`;
+  let color = '#';
+  const availableCharacters = '0123456789ABCDEF';
+  for (let i = 0; i < 6; i++) {
+    color += availableCharacters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
